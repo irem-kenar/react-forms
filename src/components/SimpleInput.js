2dee1bff704cabd63b960d56da16255abf44b4b0
Irem Kenar <ikenar@kazandirio-dev.com>
2022-04-24 19:06:22 +0300

    refactoring

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,58 +1,38 @@
-import { useRef, useState } from "react";
-
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  // if below is set to false initially, the error show up before submit
-  // if we set it false initially it would be like cheating, hence touched state below
-  const [enteredIsNameValid, setEnteredIsNameValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const handleNameInputChange = (event) => {
     setEnteredName(event.target.value);
-    // cannot use enteredName here, as it is not updated yet
-    if (event.target.value.trim() !== "") {
-      setEnteredIsNameValid(true);
-    }
   };
 
   const handleNameInputBlur = (event) => {
     setEnteredNameTouched(true);
-    if (enteredName.trim() === "") {
-      setEnteredIsNameValid(false);
-    }
   };
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
     setEnteredNameTouched(true);
 
-    if (enteredName === "") {
-      setEnteredIsNameValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
-
-    setEnteredIsNameValid(true);
-
     console.log(enteredName);
-    const enteredValue = nameInputRef.current.value;
-
-
-    //nameInputRef.current.value =""; // DON'T MANIPULATE THE DOM
-    console.log(enteredValue);
     setEnteredName("");
+    setEnteredNameTouched(false);
   };
 
-
-  const nameInputIsInvalid = !enteredIsNameValid && enteredNameTouched;
   const nameInputClasses = nameInputIsInvalid ? "form-control invalid" : "form-control";
   return (
     <form onSubmit={handleFormSubmit} >
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input
-          ref={nameInputRef}
           type='text'
           id='name'
           onChange={handleNameInputChange}

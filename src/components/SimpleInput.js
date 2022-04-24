1facbd0d0f90729a28b701dbd092feb249f870a1
Irem Kenar <ikenar@kazandirio-dev.com>
2022-04-24 14:14:03 +0300

    fetching user input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 
 
 const SimpleInput = (props) => {
-
+  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
 
   const changeInputNameHandler = (event) => {
@@ -12,13 +12,25 @@ const SimpleInput = (props) => {
   const formSubmitHandler = (event) => {
     event.preventDefault();
     console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+
+    //nameInputRef.current.value =""; // DON'T MANIPULATE THE DOM
+    console.log(enteredValue);
+    setEnteredName("");
   };
+
+
 
   return (
     <form onSubmit={formSubmitHandler} >
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={changeInputNameHandler} />
+        <input
+          ref={nameInputRef}
+          type='text'
+          id='name'
+          onChange={changeInputNameHandler}
+          value={enteredName} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
